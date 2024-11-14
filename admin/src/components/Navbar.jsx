8d4287479adc14d2d193c682e6e101d6200d24c2
nandu-2023 <152689538+nandu-2023@users.noreply.github.com>
2024-11-14 16:16:47 +0530

    Update Navbar.jsx
    'setToken' is missing in props validation
    line 4 navbar.jsx

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import PropTypes from 'prop-types'
+import { assets } from '../assets/assets'
 
-const Navbar = ({setToken}) => {
+const Navbar = ({ setToken }) => {
   return (
     <div className='flex items-center justify-between py-2 px-[4%]' >
-        <img className='w-[max(10%,80px)]' src={assets.logo} alt=""></img>
-        <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm' >Logout</button>
+        <img className='w-[max(10%,80px)]' src={assets.logo} alt="Logo" />
+        <button onClick={() => setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
+}
+
+Navbar.propTypes = {
+  setToken: PropTypes.func.isRequired,
 }
 
 export default Navbar
